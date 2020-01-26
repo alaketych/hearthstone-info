15065087d0313eebb53cb2d3bbec9f6768c875aa
Alexandr Laketych <alaketych@ukr.net>
2020-01-26 14:08:25 +0200

    mongoDB schemas

---
+++ models/Card.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const CardSchema = new Schema({
+    name    : String,
+    plrClass: {
+        type: Schema.Types.ObjectId,
+        ref : 'Class'
+    }, 
+    cardSet : {
+        type: Schema.Types.ObjectId,
+        ref : 'Set',
+    },
+    type    : String,
+    faction : String,
+    rarity  : String,
+    cost    : Number,
+    health  : Number,
+    text    : String,
+    flavor  : String,
+    artist  : String,
+    img     : String,
+    imgGold : String,
+})
+
+module.exports = mongoose.model('Card', CardSchema)
---
+++ models/Set.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const SetSchema = new Schema({
+    name    : String,
+    cards   : {
+        type: Schema.Types.ObjectId,
+        ref : 'Card',
+    }
+})
+
+module.exports = mongoose.model('Set', SetSchema)
